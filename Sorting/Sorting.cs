81e64d29cec486c687e4c6f8c4b2c3d016bdd14d
Siaukovich Kiryl <k.sevkovich@gmail.com>
2018-06-26 01:38:29 +0300

    Annotations were added.

--- Sorting/Sorting.cs
+++ Sorting/Sorting.cs
@@ -1,4 +1,6 @@
-﻿namespace Sorting
+﻿using System.Net.NetworkInformation;
+
+namespace Sorting
 {
     using System;
 
@@ -7,20 +9,41 @@
     /// </summary>
     public static class Sorting
     {
+        /// <summary>
+        /// Quicksort algorithm. Array that was passed to this function will become sorted.
+        /// </summary>
+        /// <param name="array">
+        /// Array that needs to be sorted.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// Array is null.
+        /// </exception>
         public static void Qsort(int[] array)
         {
             if (array == null)
-            {
                 throw new ArgumentNullException(nameof(array));
-            }
 
             Qsorting(array, 0, array.Length - 1);
         }
 
+        /// <summary>
+        /// Method responsible for choosing pivot element and partitioning array.
+        /// </summary>
+        /// <param name="array">
+        /// Array that needs to be sorted.
+        /// </param>
+        /// <param name="left">
+        /// Left index of sub-array.
+        /// </param>
+        /// <param name="right">
+        /// Right index of sub-array.
+        /// </param>
         private static void Qsorting(int[] array, int left, int right)
         {
             if (left > right)
+            {
                 return;
+            }
 
             var rng = new Random();
             int randomPivotIndex = rng.Next(left, right + 1);
@@ -30,6 +53,26 @@
             Qsorting(array, pivotIndex + 1, right);
         }
 
+        /// <summary>
+        /// Rearranges elements in array in the way that elements that are larger than 
+        /// the pivot are in the right side of it and elements that are lower that the
+        /// pivot are in the left side of it.
+        /// </summary>
+        /// <param name="array">
+        /// Array that needs to be sorted.
+        /// </param>
+        /// <param name="left">
+        /// Left index of sub-array.
+        /// </param>
+        /// <param name="right">
+        /// Right index of sub-array.
+        /// </param>
+        /// <param name="pivotIndex">
+        /// The pivot element index.
+        /// </param>
+        /// <returns>
+        /// The <see cref="int"/>.
+        /// </returns>
         private static int Partition(int[] array, int left, int right, int pivotIndex)
         {
             int pivot = array[pivotIndex];
@@ -49,6 +92,15 @@
             return swapPosition;
         }
 
+        /// <summary>
+        /// Swaps two elements.
+        /// </summary>
+        /// <param name="a">
+        /// First element.
+        /// </param>
+        /// <param name="b">
+        /// Second element.
+        /// </param>
         private static void Swap(ref int a, ref int b)
         {
             int temp = a;
